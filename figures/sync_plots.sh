9b0c594123b315f03801a41c5c5c5fc314c774d6
Georg Sieber <g.sieber@gmail.com>
2016-03-30 14:30:21 +0200

    new plots, first part of siljas corrections

--- figures/sync_plots.sh
+++ figures/sync_plots.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-for file in "$@"
-do
-	base_name=$(basename $file)
-	scp naf6:/nfs/dust/cms/user/gsieber/dijetana/plot/plots/$file .
-done
+# for file in "$@"
+# do
+# 	base_name=$(basename $file)
+# 	scp naf6:/nfs/dust/cms/user/gsieber/dijetana/plot/plots/$file .
+# done
+LOCALFILES=( "$@" )
+echo $LOCALFILES
+REMOTEFILES=${LOCALFILES[@]/#//nfs/dust/cms/user/gsieber/dijetana/plot/plots/}
+scp naf6:"$REMOTEFILES" .
